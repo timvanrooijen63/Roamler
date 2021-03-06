d7a872150092536f7608a60520080532c0b183e6
timvanrooijen63 <timvanrooijen63@hotmail.com>
2021-03-06 10:11:55 +0100

    feat: initialproject setup

---
+++ src/LocationFinder.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LocationFinder.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
